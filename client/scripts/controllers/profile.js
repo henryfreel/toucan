ac1e7175b336c43f88e797c900dad49bf55f2ebf
Henry Freel <henryfreel@gmail.com>
2015-09-09 18:36:26 -0700

    comments out profile pic stuff

--- client/scripts/controllers/profile.js
+++ client/scripts/controllers/profile.js
@@ -31,32 +31,32 @@ app.controller('ProfilePageCtrl', ['$scope', '$http','$routeParams', '$location'
 
   	}
 
-  	$scope.uploadPic = function(file) {
-	    file.upload = Upload.upload({
-	    	url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-	    	method: 'POST',
-	    	headers: {
-	        	'my-header': 'my-header-value'
-	    	},
-	    	fields: {username: $scope.username},
-	    	file: file,
-	    	fileFormDataName: 'myFile'
-	    });
+  	// $scope.uploadPic = function(file) {
+	  //   file.upload = Upload.upload({
+	  //   	url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+	  //   	method: 'POST',
+	  //   	headers: {
+	  //       	'my-header': 'my-header-value'
+	  //   	},
+	  //   	fields: {username: $scope.username},
+	  //   	file: file,
+	  //   	fileFormDataName: 'myFile'
+	  //   });
 
-	    file.upload.then(function (response) {
-	    	$timeout(function () {
-	        	file.result = response.data;
-	    	});
-	    }, function (response) {
-	    	if (response.status > 0)
-	        	$scope.errorMsg = response.status + ': ' + response.data;
-	    });
+	  //   file.upload.then(function (response) {
+	  //   	$timeout(function () {
+	  //       	file.result = response.data;
+	  //   	});
+	  //   }, function (response) {
+	  //   	if (response.status > 0)
+	  //       	$scope.errorMsg = response.status + ': ' + response.data;
+	  //   });
 
-	    file.upload.progress(function (evt) {
-	    	// Math.min is to fix IE which reports 200% sometimes
-	    	file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-	    });
-    }
+	  //   file.upload.progress(function (evt) {
+	  //   	// Math.min is to fix IE which reports 200% sometimes
+	  //   	file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+	  //   });
+   //  }
 
 
 
