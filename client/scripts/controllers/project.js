e3a5ccda1cba253cd6056efdff9800acc2e70e5d
Henry Freel <henryfreel@gmail.com>
2015-09-09 18:55:33 -0700

    project deletion confirmation

--- client/scripts/controllers/project.js
+++ client/scripts/controllers/project.js
@@ -109,6 +109,21 @@ app.controller('ProjectCtrl', ['$rootScope', '$scope', '$location', '$routeParam
 
 }]);
 
+app.directive('ngConfirmClick', [
+	function(){
+    	return {
+        	link: function (scope, element, attr) {
+            	var msg = attr.ngConfirmClick || "Are you sure?";
+            	var clickAction = attr.confirmedClick;
+            	element.bind('click',function (event) {
+                if ( window.confirm(msg) ) {
+                    scope.$eval(clickAction)
+                }
+            });
+        }
+    };
+}]);
+
 app.controller('ProjectsCtrl', ['$scope',function ($scope) {
 
 	$scope.project = "huh?";
